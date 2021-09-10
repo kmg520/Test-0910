834a473ff983de7bbf5dc11828625cd124b24159
kmg520 <89821254+kmg520@users.noreply.github.com>
2021-09-10 12:03:29 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,9 +7,7 @@ public class Main {
 		int b = sc.nextInt();
 		sc.close();
 		
-		double[] scores = { 85.6, 79.5, 83.1, 80.0, 78.2, 75.0 };
+		double[] scores = { 85.6, 79.5, 83.1, 80.0, 78.2, 75.0, 80.0, 90.0, 70.0, 60.0 };
 		System.out.printf("%.1f", scores[a - 1] + scores[b - 1]);
-
 	}
-
 }
